14987c27cd5aa1058d4adb2c4b57dddeb494ab7b
Carter Wilde <carterjwilde@gmail.com>
2019-06-12 11:24:53 -0600

    fixed ticket update spam

--- main.js
+++ main.js
@@ -4,27 +4,32 @@ const notifier = require('node-notifier');
 let cacheTickets = new Array();
 setInterval(() => {
     fetch("https://ticketapi.bottega.tech/tickets?role=admin")
-    .then(response => {
-        return response.json(); 
-    })
-    .then(data => { return data.tickets })
-    .then(tickets => {
-        tickets.forEach(ticket => {
-            if (cacheTickets.indexOf(ticket) == -1) {
-                cacheTickets.push(ticket);
-                //Linux sound work around.
-                console.time("audioPlay");
-                const { spawn } = require('child_process');
-                const audio = spawn('play', ['defaultBeep.ogg']);
-                console.timeEnd("audioPlay");
-                //Linux sound work around end.
+        .then(response => {
+            return response.json();
+        })
+        .then(data => { return data.tickets })
+        .then(tickets => {
+            tickets.forEach(ticket => {
+                //Cleaning up the object
+                delete ticket.assigned_users;
+                delete ticket.comments;
+                delete ticket.ticket_assignment;
+                
+                if (cacheTickets.indexOf(ticket) == -1) {
+                    cacheTickets.push(ticket);
+                    //Linux sound work around.
+                    console.time("audioPlay");
+                    const { spawn } = require('child_process');
+                    const audio = spawn('play', ['defaultBeep.ogg']);
+                    console.timeEnd("audioPlay");
+                    //Linux sound work around end.
 
-                notifier.notify({
-                    title: 'New Devcamp Ticket!',
-                    message: `A ticket from ${ticket.full_name} Title: ${ticket.title}`,
-                    sound: true
-                });                   
-            }
-        });
-    })
+                    notifier.notify({
+                        title: 'New Devcamp Ticket!',
+                        message: `A ticket from ${ticket.full_name} Title: ${ticket.title}`,
+                        sound: true
+                    });
+                }
+            });
+        })
 }, 5000);
