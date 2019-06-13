57e567c1f6042f7ee4a0631741d635bbac459a70
Carter Wilde <carterjwilde@gmail.com>
2019-06-13 11:09:18 -0600

    Added Cache Clear Time

--- main.js
+++ main.js
@@ -69,7 +69,7 @@ function ClearCache() {
         CacheTickets.pop();
     }
 }
-setInterval(ClearCache, 10000);
+setInterval(ClearCache, CacheClearTimer);
 
 function SendNotifications(ticket) {
     playSound();
